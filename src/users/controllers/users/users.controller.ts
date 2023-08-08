d18bd01af0b0b8978926457a08316afeee3afa20
FAisal Riaz <you@example.com>
2023-08-08 12:13:22 +0500

    push data using dtos

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { User } from 'src/typeorm/entities/User';
+import { UsersService } from 'src/users/services/users/users.service';
+import { ApiOperation, ApiResponse, ApiTags,ApiBody } from '@nestjs/swagger';
+
+
+@ApiTags('users') // Adds a tag to the Swagger documentation for this controller
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+//setting for swagger to give parameters
+  @Post('register')//use register as a path to post
+  @ApiOperation({ summary: 'Register a new user' })//tag the summary
+  @ApiBody({ type: User, description: 'User object with username and password' })//Show body
+  @ApiResponse({ status: 201, description: 'The user has been successfully registered.', type: User })
+  async registerUser(
+    @Body() CreateUserDto,
+   
+  ): Promise<User> {
+    return this.usersService.createUser(CreateUserDto);
+  }
+}
